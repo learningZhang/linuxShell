1ad6e001fd379a7e1f905afa664f18056e8ec2de
zhang <1554458363@qq.com>
2017-03-31 23:18:46 +0800

    fix layout

--- myshell/code/mybash.c
+++ myshell/code/mybash.c
@@ -5,94 +5,94 @@
 #include <string.h>
 #include <signal.h>
 #include <pwd.h>
+
 #define MAX_ARGV_NUM 10
 #define PATH "/home/xuanyu/Desktop/project/mybin/exec/"
 
 void printinfo();
+
 int main(int agrc, char *agrv[])
 {
    char *myargv[MAX_ARGV_NUM] = {0};
    while(1)
    {
-	memset(myargv, 0, MAX_ARGV_NUM);
+		memset(myargv, 0, MAX_ARGV_NUM);
         printinfo();
         char buff[128] = {0};
-	fgets(buff, 128, stdin);
+		fgets(buff, 128, stdin);
         buff[strlen(buff)-1] = 0;
         myargv[0] = strtok(buff, " ");
         int i = 0;
         char *s = NULL;
         while ((s = strtok(NULL, " ")) != NULL)
         {
-		myargv[++i] = s;
+			myargv[++i] = s;
         }
 
- 	int tag = 0;
-	if (myargv[0] == NULL)
-	{
-		continue;
-	}
+ 		int tag = 0;
+		if (myargv[0] == NULL)
+		{
+			continue;
+		}
         if (!(strcmp("exit", myargv[0])))
         {
-		exit(0);
+			exit(0);
         }
 
         if (!(strcmp(myargv[0], "cd")))
         {
-		if (chdir(myargv[1]) == -1)
-		{
-			printf("error\n");
-			continue;
+			if (chdir(myargv[1]) == -1)
+			{		
+				printf("error\n");
+				continue;
+			}
 		}
-	}
 
-	char path_info[256] = {0};
-	if (strncmp(buff, "/", 1)!=0 && strncmp(buff, "./", 2)!=0)
-	{
-		strcpy(path_info, PATH);
-	}
+		char path_info[256] = {0};
+		if (strncmp(buff, "/", 1)!=0 && strncmp(buff, "./", 2)!=0)
+		{
+			strcpy(path_info, PATH);
+		}
         int pid = fork();
         assert(pid != -1);
-	strcat(path_info, myargv[0]);
+		strcat(path_info, myargv[0]);
         if (pid == 0)
         {
 		//execvp(myargv[0], myargv);
-		execv(path_info, myargv);
-		printf("bash: command not found\n");
-		exit(0);
+			execv(path_info, myargv);
+			printf("bash: command not found\n");
+			exit(0);
         }
         wait(NULL);     
    }
 }
 
-
-
 void printinfo()
-{
-        uid_t uid = getuid();
-        char s = '#';
-        if (uid != 0)
+{ 
+    uid_t uid = getuid();
+    char s = '#';
+    if (uid != 0)
 	{
 		s = '$';
  	}
 
-        struct passwd *p = getpwuid(uid);
-        assert(p!=NULL);
+    struct passwd *p = getpwuid(uid);
+	assert(p!=NULL);
      
 	char hostname[128] = {0} ;
-        gethostname(hostname, 128);
+    gethostname(hostname, 128);
 	
-        char address[256] = {0};
+    char address[256] = {0};
 	if (getcwd(address, 256) == NULL)
-        {
-              perror("error");
-        }
-        char *q1 = "/"; 
+    {
+        perror("error");
+    }
+    char *q1 = "/"; 
 	char *q = strtok(address, "/");
-        while(q != NULL)
+    while(q != NULL)
 	{
 		q1 = q;
-                q = strtok(NULL,"/");
+        q = strtok(NULL,"/");
 	}
-        printf("[%s@%s %s]%c",p->pw_name, hostname, q1, s);
+    printf("[%s@%s %s]%c",p->pw_name, hostname, q1, s);
 }
